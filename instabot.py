e3a75d37df2e047bbb5010eb8e5d73b74e363945
SauciucIonut <ionut.sauciuc2003@gmail.com>
2020-12-29 15:38:26 +0200

    defining  variables and logging into instagram

---
+++ instabot.py
@@ -0,0 +1,17 @@
+# imports
+
+import os, random, shutil, glob, time
+from itertools import count
+from instabot import Bot 
+
+# initializing the bot variables
+hours = 12 # the bot will post once every set hours
+bot = Bot() # initializing the bot
+path = " " # set the path to the folder with all your pictures you want posted
+username = " " # instagram username
+password = " " # instagram password
+
+# infinite loop
+for i in count(0):
+	# logging in
+	bot.login(username = username, password = password)
